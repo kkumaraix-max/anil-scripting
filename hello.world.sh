c1e02a79337acc39aaa70c89aa5efe206ece6ee7
kathojuanil <anilkathoju@gmail.com>
2025-09-20 17:59:12 +0530

    hell

--- hello.world.sh
+++ hello.world.sh
@@ -5,11 +5,19 @@ echo "I am Anil and upskilling to DevSecOps with AWS"
 echo "Thank you"
 
 
-CITI1=Hyderabad
-CITI2=Bengalore
-CITI3=Mumbai
+#CITI1=Hyderabad
+#CITI2=Bengalore
+#CITI3=Mumbai
+
+#echo "$CITI1 Is Telangana capital"
+#echo "$CITI2 Is Karnataka capital"
+#echo "$CITI3 Is Maharastra capital"
+
+
+CITI1=$1
+CITI2=$2
+CITI3=$3
 
 echo "$CITI1 Is Telangana capital"
 echo "$CITI2 Is Karnataka capital"
 echo "$CITI3 Is Maharastra capital"
-
