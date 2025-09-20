2b0fa1aea5e85f8e7c762823f6d5a8064f637753
kathojuanil <anilkathoju@gmail.com>
2025-09-20 18:21:03 +0530

    hell

--- hello.world.sh
+++ hello.world.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #First Note#
-echo "Hello world"
-echo "I am Anil and upskilling to DevSecOps with AWS"
-echo "Thank you"
+#echo "Hello world"
+#echo "I am Anil and upskilling to DevSecOps with AWS"
+#echo "Thank you"
 
 
 #CITI1=Hyderabad
@@ -14,17 +14,19 @@ echo "Thank you"
 #echo "$CITI3 Is Maharastra capital"
 
 
-CITI1=$1
-CITI2=$2
-CITI3=$3
+#CITI1=$1
+#CITI2=$2
+#CITI3=$3
 
-echo "$CITI1 Is Telangana capital"
-echo "$CITI2 Is Karnataka capital"
-echo "$CITI3 Is Maharastra capital"
+#echo "$CITI1 Is Telangana capital"
+#echo "$CITI2 Is Karnataka capital"
+#echo "$CITI3 Is Maharastra capital"
 
-echo "Please enter your password"
-read -s PASSWORD 
-echo "password entered is : $*" #password will be hidden
+#echo "Please enter your password"
+#read -s PASSWORD 
+#echo "password entered is : $*" #password will be hidden
 #echo "password entered is : $PASSWORD" #password will displayed here
 
-echo "Course name is: $COURSE"
+#echo "Course name is: $COURSE"
+
+echo "All varibale passed to:@"
