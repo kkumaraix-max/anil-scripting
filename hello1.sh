d6857d5d501776d6b4c299da96115eedbf958c60
kathojuanil <anilkathoju@gmail.com>
2025-09-21 21:42:11 +0530

    hello

--- hello1.sh
+++ hello1.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "hello world"
+#echo "hello world"
+
+Server1="AIX"
+Server2="Linux"
+Server3="HP-UNIX"
+Server4="Solaris"
+
+echo "$Server1 is developed IBM"
+echo "$Server2 is Opensource"
+echo "$Server3 is HP"
+echo "$Server4 is Solaris1"
