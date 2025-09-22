ccabaa2a7fe0bfd222e727b85eff4295ac884b9a
kathojuanil <anilkathoju@gmail.com>
2025-09-22 19:47:41 +0530

    hello

--- installation.sh
+++ installation.sh
@@ -6,13 +6,13 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0];then
+if [ $USERID -ne 0 ]; then
     echo "you does not required access"
     exit 1
 fi    
 
 VALIDATE(){
-    if [$1 -ne 0];
+    if [ $1 -ne 0 ];
     echo "can not install $2"
     exit 1
     else 
