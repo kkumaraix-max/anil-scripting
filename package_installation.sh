1217c79e4292ece25adbdc328469dabdada10c8a
kathojuanil <anilkathoju@gmail.com>
2025-09-23 19:35:17 +0530

    hello

--- package_installation.sh
+++ package_installation.sh
@@ -6,6 +6,8 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPTNAME=$( echo $0 | cut -d "_" f1 )
 LOGFILE="$LOGS_FOLDER/$SCRIPTNAME.log"
 
+mkdir -p $LOGS_FOLDER
+
 if [ $ID -ne 0 ]; then
    echo "ERROR: Root privileges are required to execute this script ..."
    exit 1
