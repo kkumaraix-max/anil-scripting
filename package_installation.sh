af3f3f87679ac7b8a85f2d433391e4e73f7b1ee6
kathojuanil <anilkathoju@gmail.com>
2025-09-23 18:32:27 +0530

    hello

--- package_installation.sh
+++ package_installation.sh
@@ -2,13 +2,20 @@
 
 ID=$(id -u)
 
-dnf install nginx -y
-
 if [ $ID -ne 0 ]; then
    echo "you need root access"
    exit 1
-   else
-   echo "nginx installed success"
 fi
+
+dnf install nginx -y
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+      echo "Error: nginx already installed"
+      exit 1 
+    else 
+    echo "install nginx"
+   }
+ fi  
+
 
 
