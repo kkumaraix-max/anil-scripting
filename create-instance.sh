6c9b6e2eab7c82266fa3d8666eef7e8c2d114315
kathojuanil <anilkathoju@gmail.com>
2025-09-25 17:07:36 +0530

    hello

--- create-instance.sh
+++ create-instance.sh
@@ -25,21 +25,4 @@ do
 
  echo "$i: $IP" 
 
-    aws route53 change-resource-record-sets \
-  --hosted-zone-id Z096778411CYC46C0VA5Q \
-  --change-batch '
-  {
-    "Comment": "Updating record set"
-    ,"Changes": [{
-      "Action"              : "UPSERT"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "'$RECORD_NAME'"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "'$IP'"
-        }]
-      }
-    }]
-  }
-  '
+  
