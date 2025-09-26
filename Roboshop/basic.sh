e5447ac0f72b15c9671b6b617b074e0d436d2654
kathojuanil <anilkathoju@gmail.com>
2025-09-26 19:12:40 +0530

    hello

--- Roboshop/basic.sh
+++ Roboshop/basic.sh
@@ -8,16 +8,7 @@ LOG_FILE="/var/log/${UNAME}.${DATE}.log"
 if [ $ID -ne 0 ]; then
    echo "ERROR:you need root privilages to execute this command"
    exit 1
+   else
+   dnf install nginx -y &>>$DATE
+   echo "Nginx has been installed successfully"
 fi
-VALIDATE(){
-    exit_status=$1
-    package=$2
-
-if [ $1 -ne 0 ]; then
- echo "$2 exist"
- exit 1
-else
-dnf install nginx -y &>>$LOG_FILE
-echo "nginx installation cpmpleted"
-fi
-}
