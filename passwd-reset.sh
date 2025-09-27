9328550ea0d46d0a0776e6e43966389185e6da4f
kathojuanil <anilkathoju@gmail.com>
2025-09-27 22:16:33 +0530

    hello

--- passwd-reset.sh
+++ passwd-reset.sh
@@ -4,24 +4,23 @@
 #Date : 27th Sep 2025
 #Version : V1
 
-#USER=anilk
-#PASSWORD=anil@123
-
-#if [ $USER != anilk ]; then
- #   echo "ERROR: $USER is not vaialable"
-  #  else
-   # echo "$USER is exist and resetting password"
-    #echo "$USER:$PASSWORD" | chpasswd
-#fi    
-
-PASSWORD=Anil@123
-
-for USER in "$@"
-do
-if [ "$USER" = "root" ]; then
-    echo "ERROR: you should not change root user passwd"
+USER=anilk
+PASSWORD=anil@123
+if [ $USER != anilk ]; then
+    echo "ERROR: $USER is not vaialable"
     else
-    echo "$USER is Normal user you can chnage it"
+    echo "$USER is exist and resetting password"
     echo "$USER:$PASSWORD" | chpasswd
-fi
-done   
+fi    
+
+#PASSWORD=Anil@123
+
+#for USER in "$@"
+#do
+#if [ "$USER" = "root" ]; then
+#    echo "ERROR: you should not change root user passwd"
+#    else
+#    echo "$USER is Normal user you can chnage it"
+#    echo "$USER:$PASSWORD" | chpasswd
+#fi
+#done   
