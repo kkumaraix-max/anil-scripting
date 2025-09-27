c4ac7c3dfa60a0cd644ecf758ec31cd7bd26c25f
kathojuanil <anilkathoju@gmail.com>
2025-09-27 22:53:04 +0530

    hello

--- nodehealth.sh
+++ nodehealth.sh
@@ -6,6 +6,9 @@
 
 set -x #Every command will be printed before output
 
+echo "===== Disk Usage ====="
 df -h | awk -F " " '{print $4F,$5F}'  #it will print available space & used %# 
+echo "===== Memory Usage ====="
 free | awk -F " " '{print $3F,$4F}' #it will print used memory & free memory
+echo "===== CPU count ====="
 nproc # it will print no of cpu allocated to the server
