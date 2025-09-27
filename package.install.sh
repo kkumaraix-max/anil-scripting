65b59610159e4332e1468194b402ef7a1f3cc58d
kathojuanil <anilkathoju@gmail.com>
2025-09-28 00:47:34 +0530

    hello

--- package.install.sh
+++ package.install.sh
@@ -34,9 +34,9 @@ VALIDATE(){
 for package in  "$@"
 do
     dnf list installed | grep $package
-    if [ $? -ne 0 ]; then
-      dnf install $package -y &>>/var/log/$HOSTNAME.log
-        VALIDATE $? "$2"
+    if [ $1 -ne 0 ]; then
+      dnf install $package -y
+        VALIDATE $1 "$2"
     else
         echo "$package already installed in the server"
 fi
