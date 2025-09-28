6fce0246619599cdd6e45312d68bf8aedacfe3e2
kathojuanil <anilkathoju@gmail.com>
2025-09-28 18:34:13 +0530

    hello

---
+++ mysql.sh
@@ -0,0 +1,43 @@
+#!/bin/bash #ensures the script runs with the correct shell.
+#Tells the system which interpreter should run the script.
+#Here, /bin/bash means: use Bash shell to execute the script.
+#Without this, the system might try to run it with a different shell (like sh), 
+#which could break your script if you use Bash-specific features.
+#Purpose of this script is to install package # t’s just documentation for humans — 
+#explains what the script is for.
+
+#Checking Access
+id=$(id u)
+date=$(date +%d-%m-%y" "%H:%M:%S)
+log="/tmp/$HOSTNANE.$date.log"
+
+if [ $id -nq 0]; then
+    echo "ERROR: You should have superuser privilages"
+    exit 1
+    else
+    echo "Acces validated: you have necessary access,proceeding with next step"
+ fi
+
+#function to validate -- Just calling & provide input
+validate(){
+    if [ $1 -ne 0 ]; then
+    echo "error : $2 instalaltion failed"
+    exit 1
+    else
+    echo "$2 installation success"
+    fi
+}
+
+for i in "$@"
+do 
+   if dnf list installed "$i";
+   then
+    echo "$i already installed"
+    else
+    dnf install $i -y &>>$log
+    validate $? "$i"
+    fi
+done   
+   
+
+
