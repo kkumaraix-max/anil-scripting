8be2c688e158f4c5a571066c61b54c43c0eecb4e
kathojuanil <anilkathoju@gmail.com>
2025-09-29 21:01:04 +0530

    hello

--- whileloop.sh
+++ whileloop.sh
@@ -2,12 +2,23 @@
 
 #while loop for printing 1 to 100
 
-i=1
+#i=1
 
-while [ $i -lt 100 ]
-do
-    echo $i
-    i=$((i+25))
+#while [ $i -lt 100 ] ## condition 
+#do
+#    echo $i
+#    i=$((i+25)) ## command
+#done
+
+Num=100
+
+while [ $Num -gt 0 ]; then 
+do 
+  echo $Num
+  Num=$((Num-1))
+
 done
+
+
 
 
